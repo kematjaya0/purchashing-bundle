ba504ce75d9bc97d2d555db60f94fe9316bb84c1
Nur Hidayatullah <kematjaya0@gmail.com>
2021-01-31 15:04:26 +0700

    update

--- src/Repo/PurchaseDetailRepoInterface.php
+++ src/Repo/PurchaseDetailRepoInterface.php
@@ -4,11 +4,11 @@ namespace Kematjaya\PurchashingBundle\Repo;
 
 use Kematjaya\PurchashingBundle\Entity\PurchaseInterface;
 use Kematjaya\PurchashingBundle\Entity\PurchaseDetailInterface;
-use Doctrine\Common\Persistence\ObjectRepository;
+
 /**
  * @author Nur Hidayatullah <kematjaya0@gmail.com>
  */
-interface PurchaseDetailRepoInterface extends ObjectRepository 
+interface PurchaseDetailRepoInterface 
 {
     public function createObject(PurchaseInterface $purchase):PurchaseDetailInterface;
     
